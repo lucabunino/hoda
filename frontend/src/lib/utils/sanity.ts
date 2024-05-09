90b7bd7ff3a593d3ebe24b19cae02961cc30e6ca
lucabunino <66553738+lucabunino@users.noreply.github.com>
2024-05-09 18:40:57 +0200

    Update sanity.ts

--- frontend/src/lib/utils/sanity.ts
+++ frontend/src/lib/utils/sanity.ts
@@ -10,7 +10,7 @@ if (!PUBLIC_SANITY_PROJECT_ID || !PUBLIC_SANITY_DATASET) {
 export const client = createClient({
 	projectId: PUBLIC_SANITY_PROJECT_ID,
 	dataset: PUBLIC_SANITY_DATASET,
-	useCdn: false, // `false` if you want to ensure fresh data
+	useCdn: true, // `false` if you want to ensure fresh data
 	apiVersion: '2024-03-16' // date of setup
 });
 
